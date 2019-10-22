0e02d988ad2ef1bf2161790c1639334d69510455
Chris Lin <chris4540@gmail.com>
2019-10-22 22:27:21 +0200

    Code refactoring

--- train_zeroshot.py
+++ train_zeroshot.py
@@ -5,7 +5,7 @@ tf.compat.v1.enable_eager_execution(config=None, device_policy=None,execution_mo
 from net.generator import NavieGenerator
 from utils.cosine_anealing import CosineAnnealingScheduler
 from utils.losses import kd_loss
-from utils.losses import student_loss
+from utils.losses import student_loss_fn
 from tensorflow.keras.optimizers import Adam
 from net.wide_resnet import WideResidualNetwork
 import numpy as np
@@ -55,15 +55,15 @@ for total_batches in range(total_n_pseudo_batches):
     for ng in range(ng_batches):
         with tf.GradientTape() as tape:
             pseudo_imgs = generator(z)
-            t_logits, *t_acts = teacher(pseudo_imgs)
-            s_logits, *s_acts = student(pseudo_imgs)
+            t_logits, *_ = teacher(pseudo_imgs)
+            s_logits, *_ = student(pseudo_imgs)
 
             # calculate the generator loss
-            generator_loss = kd_loss(tf.math.softmax(t_logits),
+            loss = kd_loss(tf.math.softmax(t_logits),
                                      tf.math.softmax(s_logits))
 
         # The grad for generator
-        grads = tape.gradient(generator_loss, generator.trainable_weights)
+        grads = tape.gradient(loss, generator.trainable_weights)
 
         # cosine annealing for learning rate
         generator_optimizer.learning_rate = cosine_lr_schedule(total_batches, total_n_pseudo_batches, generator_lr)
@@ -71,7 +71,7 @@ for total_batches in range(total_n_pseudo_batches):
         # update the generator paramter with the gradient
         generator_optimizer.apply_gradients(zip(grads, generator.trainable_weights))
 
-        g_loss_met(generator_loss)
+        g_loss_met(loss)
 
         if total_batches % 2 == 0:
             print('step %s: generator mean loss = %s' % (total_batches, g_loss_met.result()))
@@ -85,16 +85,16 @@ for total_batches in range(total_n_pseudo_batches):
         t_logits, *t_acts = teacher(pseudo_imgs)
         with tf.GradientTape() as tape:
             s_logits, *s_acts = student(pseudo_imgs)
-            std_loss = student_loss(t_logits, t_acts, s_logits, s_acts, attn_beta)
+            loss = student_loss_fn(t_logits, t_acts, s_logits, s_acts, attn_beta)
 
         # The grad for student
-        grads = tape.gradient(std_loss, student.trainable_weights)
+        grads = tape.gradient(loss, student.trainable_weights)
 
         # Update learning rate
         student_optimizer.learning_rate = cosine_lr_schedule(total_batches, total_n_pseudo_batches, student_lr)
         student_optimizer.apply_gradients(zip(grads, student.trainable_weights))
 
-        stu_loss_met(std_loss)
+        stu_loss_met(loss)
 
         if total_batches % 2 == 0:
             print('step %s: studnt mean loss = %s' % (total_batches, stu_loss_met.result()))
