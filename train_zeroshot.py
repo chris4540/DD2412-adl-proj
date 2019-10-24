2aebcf7cb31825bdce63e2aa6c6d44e1658218cf
vivek9260 <ch.vivek92@gmail.com>
2019-10-25 01:57:57 +0200

    zeroshot

--- train_zeroshot.py
+++ train_zeroshot.py
@@ -29,15 +29,13 @@ from net.generator import NavieGenerator
 from utils.losses import kd_loss
 from utils.losses import student_loss_fn
 from utils.preprocess import load_cifar10_data
+from utils.csvlogger import CustomizedCSVLogger
 from tensorflow.keras.optimizers import Adam
 from net.wide_resnet import WideResidualNetwork
 from tensorflow.keras.experimental import CosineDecay
 import numpy as np
 import os
-import logging
 
-logging.basicConfig(level=logging.INFO, format='%(message)s')
-logger = logging.getLogger()
 
 # TODO: use Config class
 class Config:
@@ -62,11 +60,8 @@ class Config:
     student_init_lr = 2e-3
     generator_init_lr = 1e-3
 
-    t_depth = 40
-    t_width = 2
-
-    s_depth = 16
-    s_width = 2
+    # generator 
+    save_models_at = 1000
 
 
 def mkdir(dirname):
@@ -78,20 +73,16 @@ def zeroshot_train(t_depth, t_width, t_path, s_depth=16, s_width=1, seed=42, sav
 
     set_seed(seed)
 
-    model_config = '%s_T-%d-%d_S-%d-%d_%d' % (dataset, t_depth, t_width, s_depth, s_width, seed)
-    model_name = '%s_model.h5' % model_config
+    model_config = '%s_T-%d-%d_S-%d-%d_seed_%d' % (dataset, t_depth, t_width, s_depth, s_width, seed)
+    #model_name = '%s_model.h5' % model_config
     log_filename = model_config + '_training_log.csv'
 
     save_dir = os.path.join(os.getcwd(), savedir)
     mkdir(save_dir)
 
-    model_filepath = os.path.join(save_dir, model_name)
+    #model_filepath = os.path.join(save_dir, model_name)
     log_filepath = os.path.join(save_dir, log_filename)
-
-    logging.basicConfig(level=logging.INFO, format='%(message)s')
-    logger = logging.getLogger()
-    logger.addHandler(logging.FileHandler(log_filepath, 'a'))
-    logger.info("Iteration,Generator_Loss,Student_Loss,Student_Test_Loss,Student_Test_Accuracy")
+    logger = CustomizedCSVLogger(log_filepath)
 
     ## Teacher
     teacher = WideResidualNetwork(t_depth, t_width,
@@ -109,6 +100,7 @@ def zeroshot_train(t_depth, t_width, t_path, s_depth=16, s_width=1, seed=42, sav
                                   dropout_rate=0.0,
                                   output_activations=True,
                                   has_softmax=False)
+
     student_optimizer = Adam(learning_rate=CosineDecay(
                                 Config.student_init_lr,
                                 decay_steps=Config.n_outer_loop*Config.n_g_in_loop))
@@ -155,6 +147,7 @@ def zeroshot_train(t_depth, t_width, t_path, s_depth=16, s_width=1, seed=42, sav
 
             if iter_ % 50 == 0:
                 print('step %s: generator mean loss = %s' % (iter_, g_loss_met.result().numpy()))
+
         # ==========================================================================
 
         # Student training
@@ -180,17 +173,23 @@ def zeroshot_train(t_depth, t_width, t_path, s_depth=16, s_width=1, seed=42, sav
 
         if (iter_ + 1) % (Config.n_outer_loop/200) == 0:
             test_loss, test_accuracy = get_accuracy(student, x_test, y_test)
-            logger.info(iter_,gen_loss.numpy(),stu_loss.numpy(), test_loss, test_accuracy)
+            logger.log(Iteration=iter_, Generator_loss=gen_loss.numpy(), 
+                Student_loss=stu_loss.numpy(), Test_loss=test_loss, Test_acuracy=test_accuracy)
 
-    student.save(model_filepath)
+        if (iter_ + 1) % Config.save_models_at == 0:
+                generator_name = '%s_generator_itr_%d.h5' % (model_config, iter_)
+                generator_filepath = os.path.join(save_dir, generator_name)
+                student_name = '%s_student_itr_%d.h5' % (model_config, iter_)
+                student_filepath = os.path.join(save_dir, student_name)
+                generator.save(generator_filepath)
+                student.save(student_filepath)
 
 
 def get_accuracy(student_model, s_depth, s_width, x_test, y_test):
     model = WideResidualNetwork(s_depth, s_width, input_shape=(32, 32, 3), dropout_rate=0.0)
     model.set_weights(student_model.get_weights()) 
-    model.compile(loss='categorical_crossentropy',
-                      metrics=['accuracy'])
-    loss, accuracy = model.evaluate(x_test, y_test, batch_size=128, verbose=0)
+    model.compile(loss='categorical_crossentropy', metrics=['accuracy'])
+    loss, accuracy = model.evaluate(x_test, y_test, batch_size=200, verbose=0)
     return loss, accuracy
 
 
