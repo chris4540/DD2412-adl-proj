452c68d08c8e33059f272ac229ccb2bfa0256069
Chris Lin <chris4540@gmail.com>
2019-10-25 08:32:51 +0200

    Update

--- run_exp1_kdat.sh
+++ run_exp1_kdat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Teacher wrn-40-2; Student wrn-16-1
-python train_kdat.py --tdepth=40 --twidth=2 --sdepth=16 --swidth=1 \
-                     -m 200 \
-                     --teacher_weights=saved_models/cifar10_WRN-40-2_model.h5 \
-                     --dataset="cifar10"
+python3 train_kdat.py --tdepth=40 --twidth=2 --sdepth=16 --swidth=1 \
+                      -m 200 \
+                      --teacher_weights=saved_models/cifar10_WRN-40-2_model.h5 \
+                      --dataset="cifar10"
