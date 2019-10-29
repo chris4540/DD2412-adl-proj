219192e4f211454a34469a3633fa328ae66f821c
Chris Lin <chris4540@gmail.com>
2019-10-29 22:45:24 +0000

    Bug fix

--- run_exp1_kdat.sh
+++ run_exp1_kdat.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # # Teacher wrn-40-2; Student wrn-16-1
-# python3 train_kdat.py -td 40 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgt ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 10
-# python3 train_kdat.py -td 40 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgt ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 23
-# # python3 train_kdat.py -td 40 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgt ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 45
+# python3 train_kdat.py -td 40 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgts ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 10
+# python3 train_kdat.py -td 40 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgts ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 23
+# # python3 train_kdat.py -td 40 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgts ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 45
 
 
 #Teacher wrn-16-2; Student wrn-16-1
-python3 train_kdat.py -td 16 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgt ~/cifar10_WRN-16-2-seed45_model.181.h5 --seed 10
-python3 train_kdat.py -td 16 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgt ~/cifar10_WRN-16-2-seed45_model.181.h5 --seed 23
-python3 train_kdat.py -td 16 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgt ~/cifar10_WRN-16-2-seed45_model.181.h5 --seed 45
+python3 train_kdat.py -td 16 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgts ~/cifar10_WRN-16-2-seed45_model.181.h5 --seed 10
+python3 train_kdat.py -td 16 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgts ~/cifar10_WRN-16-2-seed45_model.181.h5 --seed 23
+python3 train_kdat.py -td 16 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgts ~/cifar10_WRN-16-2-seed45_model.181.h5 --seed 45
 
 # Teacher wrn-40-2; Student wrn-40-1
-python3 train_kdat.py -td 40 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgt ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 10
-python3 train_kdat.py -td 40 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgt ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 23
-python3 train_kdat.py -td 40 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgt ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 45
+python3 train_kdat.py -td 40 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgts ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 10
+python3 train_kdat.py -td 40 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgts ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 23
+python3 train_kdat.py -td 40 -tw 2 -sd 16 -sw 1 -m 200  --dataset=cifar10 -twgts ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 45
 
 # Teacher wrn-40-2; Student wrn-40-1
-python3 train_kdat.py -td 40 -tw 2 -sd 40 -sw 1 -m 200  --dataset=cifar10 -twgt ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 10
-python3 train_kdat.py -td 40 -tw 2 -sd 40 -sw 1 -m 200  --dataset=cifar10 -twgt ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 23
-python3 train_kdat.py -td 40 -tw 2 -sd 40 -sw 1 -m 200  --dataset=cifar10 -twgt ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 45
+python3 train_kdat.py -td 40 -tw 2 -sd 40 -sw 1 -m 200  --dataset=cifar10 -twgts ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 10
+python3 train_kdat.py -td 40 -tw 2 -sd 40 -sw 1 -m 200  --dataset=cifar10 -twgts ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 23
+python3 train_kdat.py -td 40 -tw 2 -sd 40 -sw 1 -m 200  --dataset=cifar10 -twgts ~/cifar10_WRN-40-2-seed45_model.172.h5 --seed 45
