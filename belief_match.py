7fd81bede95646ac4647e76ba6b01440ce443ed4
Chris Lin <chris4540@gmail.com>
2019-10-30 22:58:59 +0000

    Add calculation of same pred

--- belief_match.py
+++ belief_match.py
@@ -5,12 +5,14 @@ import tensorflow as tf
 tf.enable_v2_behavior()
 from net.wide_resnet import WideResidualNetwork
 from utils.preprocess import get_cifar10_data
+from utils.preprocess import balance_sampling
 
 if __name__ == "__main__":
     # data
     (_, _), (x_test, y_test_labels) = get_cifar10_data()
-    y_train = to_categorical(y_train_lbl)
-    test_data_loader = tf.data.Dataset.from_tensor_slices((x_test, y_test_lbl)).batch(200)
+    if True:
+        x_test, y_test_labels = balance_sampling(x_test, y_test_labels, data_per_class=50)
+    test_data_loader = tf.data.Dataset.from_tensor_slices((x_test, y_test_labels)).batch(200)
 
     # Teacher
     teacher = WideResidualNetwork(40, 2, input_shape=(32, 32, 3))
@@ -25,9 +27,26 @@ if __name__ == "__main__":
     teacher.trainable = False
     # ========================================================
     # ========================================================
-    # Make prediction
-    t_pred = tf.argmax(teacher(x_test), -1)
-    s_pred = tf.argmax(student(x_test), -1)
+    # check if matched of two model predictions
+    # use mini-batch for memory issue
+    same_pred_idx_list = []
+    offset = 0
+    for batch_x, batch_y_lbl in test_data_loader:
+        # Make prediction
+        t_pred = tf.argmax(teacher(batch_x), -1)
+        s_pred = tf.argmax(student(batch_x), -1)
 
-    # check if matched
-    common_pred = tf.reshape(tf.where(s_pred == t_pred), [-1])
+
+        same_pred = tf.compat.v2.where(tf.equal(t_pred, s_pred))
+        same_pred = tf.cast(same_pred, tf.int32)
+        same_pred = tf.reshape(same_pred, [-1]) + offset
+
+        # add back offset
+        offset += tf.size(batch_y_lbl)
+
+        same_pred_idx_list.append(same_pred)
+
+    same_pred_idxs = tf.concat(same_pred_idx_list, 0)
+    n_match_data = tf.size(same_pred_idxs).numpy()
+    print("# of testing data for matching belief = ", n_match_data)
+    # --------------------------------------------------------
