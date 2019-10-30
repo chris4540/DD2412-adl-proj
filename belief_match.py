c4ac7e63c562d7ef87296179bca45d3d6da181ef
Chris Lin <chris4540@gmail.com>
2019-10-30 21:39:05 +0000

    belief match wip

--- belief_match.py
+++ belief_match.py
@@ -3,10 +3,13 @@ Refactor from ryan code
 """
 from utils.preprocess import get_cifar10_data
 from net.wide_resnet import WideResidualNetwork
+import tensortflow as tf
 
 if __name__ == "__main__":
     # data
     (_, _), (x_test, y_test_labels) = get_cifar10_data()
+    y_train = to_categorical(y_train_lbl)
+    test_data_loader = tf.data.Dataset.from_tensor_slices((x_test, y_test_lbl)).batch(200)
 
     # Teacher
     teacher = WideResidualNetwork(40, 2, input_shape=(32, 32, 3))
