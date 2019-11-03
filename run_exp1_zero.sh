64a5dc6c0c9716e3d3199f81d998692443c2089e
Chris Lin <chris4540@gmail.com>
2019-11-03 05:58:56 +0100

    Update run zero script

--- run_exp1_zero.sh
+++ run_exp1_zero.sh
@@ -14,3 +14,9 @@ python3 train_zeroshot.py -tw 2 -td 40 -sw 1 -sd 40 -twgt ~/cifar10_WRN-40-2-see
 python3 train_zeroshot.py -tw 2 -td 40 -sw 1 -sd 16 -twgt ~/cifar10_WRN-40-2-seed10_model.204.h5  --seed 10
 python3 train_zeroshot.py -tw 2 -td 40 -sw 1 -sd 16 -twgt ~/cifar10_WRN-40-2-seed23_model.204.h5  --seed 23
 python3 train_zeroshot.py -tw 2 -td 40 -sw 1 -sd 16 -twgt ~/cifar10_WRN-40-2-seed45_model.204.h5  --seed 45
+# ==========================================================================
+
+# WRN-40-2 => WRN-16-2
+python3 train_zeroshot.py -tw 2 -td 40 -sw 2 -sd 16 -twgt ~/cifar10_WRN-40-2-seed10_model.204.h5  --seed 10
+python3 train_zeroshot.py -tw 2 -td 40 -sw 2 -sd 16 -twgt ~/cifar10_WRN-40-2-seed23_model.204.h5  --seed 23
+python3 train_zeroshot.py -tw 2 -td 40 -sw 2 -sd 16 -twgt ~/cifar10_WRN-40-2-seed45_model.204.h5  --seed 45
