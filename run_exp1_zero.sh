3525506eb51aad0aa0f434b11a1c161b952cacbd
Chris Lin <chris4540@gmail.com>
2019-11-03 09:36:43 +0100

    Update exp run zero script

--- run_exp1_zero.sh
+++ run_exp1_zero.sh
@@ -5,6 +5,7 @@ python3 train_zeroshot.py -tw 2 -td 16 -sw 1 -sd 16 -twgt ~/cifar10_WRN-16-2-see
 python3 train_zeroshot.py -tw 2 -td 16 -sw 1 -sd 16 -twgt ~/cifar10_WRN-16-2-seed23_model.204.h5 --seed 23
 python3 train_zeroshot.py -tw 2 -td 16 -sw 1 -sd 16 -twgt ~/cifar10_WRN-16-2-seed45_model.204.h5 --seed 45
 # ==========================================================================
+# V100-preemp
 # WRN-40-2 => WRN-40-1
 python3 train_zeroshot.py -tw 2 -td 40 -sw 1 -sd 40 -twgt ~/cifar10_WRN-40-2-seed10_model.204.h5  --seed 10
 python3 train_zeroshot.py -tw 2 -td 40 -sw 1 -sd 40 -twgt ~/cifar10_WRN-40-2-seed23_model.204.h5  --seed 23
@@ -15,8 +16,17 @@ python3 train_zeroshot.py -tw 2 -td 40 -sw 1 -sd 16 -twgt ~/cifar10_WRN-40-2-see
 python3 train_zeroshot.py -tw 2 -td 40 -sw 1 -sd 16 -twgt ~/cifar10_WRN-40-2-seed23_model.204.h5  --seed 23
 python3 train_zeroshot.py -tw 2 -td 40 -sw 1 -sd 16 -twgt ~/cifar10_WRN-40-2-seed45_model.204.h5  --seed 45
 # ==========================================================================
-
+# tf-exp-p100
 # WRN-40-2 => WRN-16-2
 python3 train_zeroshot.py -tw 2 -td 40 -sw 2 -sd 16 -twgt ~/cifar10_WRN-40-2-seed10_model.204.h5  --seed 10
 python3 train_zeroshot.py -tw 2 -td 40 -sw 2 -sd 16 -twgt ~/cifar10_WRN-40-2-seed23_model.204.h5  --seed 23
 python3 train_zeroshot.py -tw 2 -td 40 -sw 2 -sd 16 -twgt ~/cifar10_WRN-40-2-seed45_model.204.h5  --seed 45
+# ==========================================================================
+# WRN-40-1 => WRN-16-2
+python3 train_zeroshot.py -tw 1 -td 40 -sw 2 -sd 16 -twgt ~/cifar10_WRN-40-1-seed10_model.204.h5  --seed 10
+python3 train_zeroshot.py -tw 1 -td 40 -sw 2 -sd 16 -twgt ~/cifar10_WRN-40-1-seed23_model.204.h5  --seed 23
+python3 train_zeroshot.py -tw 1 -td 40 -sw 2 -sd 16 -twgt ~/cifar10_WRN-40-1-seed45_model.204.h5  --seed 45
+# WRN-40-1 => WRN-16-1
+python3 train_zeroshot.py -tw 1 -td 40 -sw 1 -sd 16 -twgt ~/cifar10_WRN-40-1-seed10_model.204.h5  --seed 10
+python3 train_zeroshot.py -tw 1 -td 40 -sw 1 -sd 16 -twgt ~/cifar10_WRN-40-1-seed23_model.204.h5  --seed 23
+python3 train_zeroshot.py -tw 1 -td 40 -sw 1 -sd 16 -twgt ~/cifar10_WRN-40-1-seed45_model.204.h5  --seed 45
